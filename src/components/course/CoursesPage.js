2453483dce11d19bbb221eaad9a8b15d6c01a855
softtech\kemal.bayer <kemal.bayer@softtech.com.tr>
2017-01-24 23:39:56 +0300

    first commit

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,12 @@
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Courses</h1>
+            </div>
+        );    
+    }
+}
+export default CoursesPage;
